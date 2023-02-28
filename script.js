592686e7440958b79d274a0c51d9fbf7bbba006c
GOWTHAM KANAGARAJ <113981482+GowthamKanagaraj@users.noreply.github.com>
2023-02-28 23:35:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,101 @@
+const inputEl=document.getElementById('crud');
+const formEl = document.getElementById('form-item');
+const btnEl=document.getElementById('btn');
+const containerEl=document.getElementById('todo-list-container');
+//global variables
+let tasks=[];
+let editId=null;
+let isEditing=false;
+
+//init
+function init(){
+    isEditing=false;
+    editId=null;
+    btnEl.innerText='Submit';
+}
+init();
+
+//read ui  //step-3 read
+const readUi=function(){
+    containerEl.innerHTML=null;//if we delete
+    inputEl.value=null;//after update
+
+
+    tasks.forEach((task)=>{
+    const taskEl=document.createElement('li');
+
+    taskEl.innerHTML= `${task.taskName}
+    <button class="btn-update"onclick = updateItem(${task.id}) ><i class="fa-sharp fa-regular fa-pen-to-square"></i></button>
+    <button class="btn-delete" onclick = deleteItem(${task.id})><i class="fa-sharp fa-solid fa-trash"></i></button>`;
+    containerEl.appendChild(taskEl);
+    })  
+}
+//delete //step-4
+const deleteItem=function(id){
+    console.log(id)
+    tasks =tasks.filter(taskDelete =>{
+        return taskDelete.id !== id ;//checking the tasks.id with onclick task id
+    })
+    readUi();
+}
+
+//update //step-5
+    const updateItem=function(id){
+        isEditing = true;
+        btnEl.innerText='update';
+
+//find the elements
+        const itemToEdit = tasks.find((taskUpdate)=>{
+            return taskUpdate.id === id;//checking the tasks.id with onclick task id
+        });
+        inputEl.value = itemToEdit.taskName;
+        editId = itemToEdit.id;
+    }
+
+//input & submit
+//step-1 submit
+formEl.addEventListener('submit', function(event){
+    event.preventDefault();
+    const title = inputEl.value;
+
+//step 6
+if(isEditing){
+    tasks=tasks.map((task)=>{
+        if(task.id===editId)
+        {
+            return{
+                id:editId,
+                taskName:title,
+            };
+        }else{
+            return task;
+        }
+    })
+    init();
+    readUi();
+}
+//step-2 create
+    else{
+        if (inputEl.value ==='') 
+        {
+        alert("Please Enter a Valid value");
+        }
+        else{
+    const task={
+        id:Date.now(),
+        taskName:title,
+    }
+    console.log(task)
+
+    //add task  to tasks array
+    tasks.push(task)
+    console.log(tasks)
+
+    //display
+    readUi();
+
+    //clean the input
+    inputEl.value=null;//after typing
+}
+}
+});
